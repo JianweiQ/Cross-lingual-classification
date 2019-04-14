7154d24a5f17fe353a355b8aba8ead027d287422
Yixin Peng <ypeng27@hawk.iit.edu>
2019-04-13 20:26:41 -0500

    Added UM-Corpus dataset

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Cross-lingual-classification
 Cross-lingual Embeddings and Text Classification
+
+# Packages
+nltk
+sklearn
+thulac
+numpy
+
+# Pre-data
+nltk.download('wordnet')
+sklearn.fetch_20newsgroups()
+
+# Inputs
+Put wiki.zh.align.vec wiki.en.align.vec in subfolder data
+Put Chinese corpus in subfolder data/sport data/politics data/science
+put UM corpus txt in subfolder data/UM-Corpus
--- main.py
+++ main.py
@@ -19,22 +19,31 @@ from sklearn.linear_model import LogisticRegression
 # import nltk
 # nltk.download('wordnet')
 
+MAX_LINE = 100000
+
 
 def main():
     warnings.filterwarnings("ignore", category=FutureWarning)
+    model = 1  # 1 for THU, 2 for UM-corpus
+    # English corpus loading
+    if model == 1:
+        cat = ['rec.sport.baseball', 'talk.politics.misc', 'sci.electronics'] 
+    #     # More cat lead to worse results
+#         cat = ['rec.sport.baseball', 'rec.sport.hockey',
+#                'talk.politics.mideast', 'talk.politics.guns', 'talk.politics.misc',
+#                'sci.crypt', 'sci.electronics', 'sci.med', 'sci.space'] 
+        dataset = fetch20newsgroup(cat, "all")
+        X = dataset.data
+        y = dataset.target
+#         y = relabel20newsgroup(y)
+    elif model == 2:  # 1 for THU, 2 for UM-corpus
+        X, y, X_c, y_c = fetchUMCorpus()
+        
     # English w2v
-    # Yixin: Put wiki.zh.align.vec wiki.en.align.vec in subfolder data
-    cat = ['rec.sport.baseball', 'talk.politics.misc', 'sci.electronics'] 
-#     # More cat lead to worse results
-#     cat = ['rec.sport.baseball', 'rec.sport.hockey',
-#            'talk.politics.mideast', 'talk.politics.guns', 'talk.politics.misc',
-#            'sci.crypt', 'sci.electronics', 'sci.med', 'sci.space'] 
-    englishNews = fetch20newsgroup(cat, "all")
-    englishWords = tokenizeEnglish(englishNews)
-    w2v_en = loadWordVectors('data/wiki.en.align.vec', 10000)  # Yixin: can be slow, try 10000 first
-    X = featurizeEnglish(englishWords, w2v_en)
-    y = englishNews.target
-#     y = relabel(englishNews.target)
+    X = tokenizeEnglish(X)
+    w2v_en = loadWordVectors('data/wiki.en.align.vec', MAX_LINE)  
+#     w2v_en = loadWordVectors('data/vectors-en.txt', MAX_LINE)
+    X = featurizeEnglish(X, w2v_en)
       
     # English classifiers
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
@@ -42,10 +51,10 @@ def main():
     classifiers(X_train, y_train, X_test, y_test)
      
     # Chinese w2v
-    # Yixin: Put Chinese corpus in subfolder data/sport data/politics data/science
-#     chineseWordSegmentation() # run only once to get output
-    X_c, y_c = tokenizeChinese()
-    w2v_zh = loadWordVectors('data/wiki.zh.align.vec', 10000)  
+    if model == 1:
+        X_c, y_c = fetchTHUnews(1000)
+    w2v_zh = loadWordVectors('data/wiki.zh.align.vec', MAX_LINE)  
+#     w2v_zh = loadWordVectors('data/vectors-zh.txt', MAX_LINE)  
     X_c = featurizeChinese(X_c, w2v_zh)
     
     # Chinese classifiers
@@ -60,17 +69,7 @@ def main():
     # Chinese classifiers to train English vectors
     print("Chinese training vectors for English testing vectors:")
     classifiers(X_c_train, y_c_train, X, y)
-    
-def relabel(target):
-    y = []
-    for t in target:
-        if t == 0 or t == 1:
-            y.append(0)
-        elif t == 2 or t == 3 or t == 4:
-            y.append(1)
-        else:
-            y.append(2)
-    return y
+
     
 def fetch20newsgroup(cat, subset):
     """ param: cat, a list of categories
@@ -84,10 +83,121 @@ def fetch20newsgroup(cat, subset):
     return dataset
 
 
-def tokenizeEnglish(news):
-    """param: news, a list of strings
+def relabel20newsgroup(target):
+    y = []
+    for t in target:
+        if t == 0 or t == 1:
+            y.append(0)
+        elif t == 2 or t == 3 or t == 4:
+            y.append(1)
+        else:
+            y.append(2)
+    return y
+
+
+def fetchTHUnews(size=float("inf")):
+
+    def read_files(file_name, target, X, y):
+        """param: file_name, string type file directory
+        param: target, int for target label
+        param: X to append
+        param: y to append"""
+        cur = 0
+        for file in os.listdir(file_name):
+            doc = []
+            file = open(file_name + "/" + file, 'r')
+            for line in file:
+                l = line.split()
+                doc = doc + l
+            X.append(doc)
+            y.append(target)
+            if cur >= size: break
+            else: cur += 1
+
+    print('fetchTHUnews...')
+    X = []
+    y = []
+    read_files("data/sport_out", 0, X, y)
+    read_files("data/politics_out", 1, X, y)
+    read_files("data/science_out", 2, X, y)
+    return (X, y)
+
+
+def THUWordSegmentation():
+    """One time function to separate Chinese by space and save"""
+    print('THUWordSegmentation...')
+    thu = thulac.thulac(seg_only=True)  # segment only
+    # sport 0-131603.txt
+    # politics 339764-402849.txt
+    # science 481650-644578.txt
+    for i in range(50000, 60000):
+        if (i / 10) == int(i / 10): print(i)
+        filename = str(i)
+        # TODO pick 1000 files only for speed
+        thu.cut_f("data/sport/" + filename + ".txt", "data/sport_out/" + filename + ".txt")
+        thu.cut_f("data/politics/3" + filename + ".txt", "data/politics_out/3" + filename + ".txt")
+        thu.cut_f("data/science/5" + filename + ".txt", "data/science_out/5" + filename + ".txt")
+
+
+def fetchUMCorpus():
+
+    def read_files(file_name, target, X, y, is_token):
+        """param: file_name, string type file name
+        param: target, int for target label
+        param: X to append
+        param: y to append"""
+        file = open(file_name, 'r')
+        for line in file:
+            if is_token == 1:
+                l = line.split()
+            else:
+                l = line
+            X.append(l)
+            y.append(target)
+            
+    print('fetchUMCorpus...')
+    X_en, y_en, X_zh, y_zh = [], [], [], []
+    read_files("data/UM-Corpus/en/Bi-Laws.txt", 0, X_en, y_en, 0)
+    read_files("data/UM-Corpus/en/Bi-Thesis.txt", 1, X_en, y_en, 0)
+    read_files("data/UM-Corpus/zh_s/Bi-Laws.txt", 0, X_zh, y_zh, 1)
+    read_files("data/UM-Corpus/zh_s/Bi-Thesis.txt", 1, X_zh, y_zh, 1)
+    return (X_en, y_en, X_zh, y_zh)
+
+
+def UMWordSegmentation(filenames, size=float("inf")):
+    """param: filenames, filename to deal with
+    param: size, max size of lines to perform
+    One time function to separate English and Chinese,
+    and separate Chinese by space and save"""
+    print('UMWordSegmentation...')
+    # separate English and Chinese 
+    for filename in filenames:
+        # Bi-Laws 440000 Bi-Thesis 600000
+        input_file = "data/UM-Corpus/" + filename + ".txt"
+        output_file_en = "data/UM-Corpus/en/" + filename + ".txt"
+        output_file_zh = "data/UM-Corpus/zh/" + filename + ".txt"
+        i_file = open(input_file, 'r')
+        o_file_en = open(output_file_en, 'a')
+        o_file_zh = open(output_file_zh, 'a')
+        cur = 0
+        for line in i_file:
+            if (cur / 2) == int(cur / 2):  # even for English
+                o_file_en.write(line)
+            else:
+                o_file_zh.write(line)
+            if cur >= size: break
+            else: cur += 1
+        o_file_en.close()
+        o_file_zh.close()
+    # separate Chinese words
+    thu = thulac.thulac(seg_only=True)  # segment only
+    for file in os.listdir("data/UM-Corpus/zh"):
+        thu.cut_f("data/UM-Corpus/zh/" + file, "data/UM-Corpus/zh_s/" + file)
+
+
+def tokenizeEnglish(docs):
+    """param: docs, a list of strings
     return: a list of lists of tokens (words)"""
-    print('tokenizeEnglish...')
 
     class LemmaTokenizer(object):  # for stemming and lemmatizing
 
@@ -99,32 +209,19 @@ def tokenizeEnglish(news):
             doc = re.sub(r'\S*@\S*\s?', '', doc.lower())  # to lower case, remove emails
             doc = ''.join([i for i in doc if not i.isdigit()])  # remove digits/numbers
             return [self.wnl.lemmatize(t) for t in tokenizer.tokenize(doc)]
-
+    
+    print('tokenizeEnglish...')
     # vectorizer = TfidfVectorizer(stop_words='english', max_df=0.3, min_df=5, tokenizer=LemmaTokenizer())
     # X = vectorizer.fit_transform(englishNews.data)
     # y = englishNews.target
     tokenizer = LemmaTokenizer()
-    docs = [tokenizer(doc) for doc in news.data]
-    return docs
+    ret = [tokenizer(doc) for doc in docs]
+    return ret
 
 
-def loadWordVectors(fname, size=float('inf')):
-    """param: fname, file name of word embeddings
-    size: for max loaded lines restriction
-    return: a dictionary storing the embeddings"""
-    print('loadWordVectors:' + fname + '...')
-    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-    n, d = map(int, fin.readline().split())
-    print(n, d)  # n is #words, d is dimension
-    # fin.readline()
-    data = {}
-    cur = 0
-    for line in fin:
-        tokens = line.rstrip().split(' ')
-        data[tokens[0]] = np.asarray(list(map(float, tokens[1:])))  # Yixin: added np.asarray(list())
-        if (cur > size): break 
-        else: cur += 1
-    return data
+def tokenizeChinese():
+    print('tokenizeChinese...')
+    # TODO remove frequence, stop words, punctuation
 
 
 def featurizeEnglish(docs, w2v):
@@ -169,6 +266,69 @@ def featurizeEnglish(docs, w2v):
     return X
 
 
+def featurizeChinese(docs, w2v):
+    """param: docs, a list of lists of tokens (strings)
+    w2v, a dictionary of word embeddings
+    return: a matrix, a row represents a doc's feature vector"""
+    
+    class MeanEmbeddingVectorizer(object):
+
+        def __init__(self, word2vec, dim=300):
+            self.word2vec = word2vec
+            # if a text is empty we should return a vector of zeros
+            # with the same dimensionality as all the other vectors
+    #         self.dim = len(word2vec.itervalues().next())
+            self.dim = dim
+    
+        def fit(self, X):
+            return self
+    
+        def transform(self, X):
+            hit = 0
+            count = 0
+            retl = []
+            for doc in X:
+                l = []
+                for word in doc:
+                    count += 1
+                    if word in self.word2vec:
+                        l.append(self.word2vec[word])
+                        hit += 1           
+                retl.append(np.mean(l or np.zeros(self.dim), axis=0))
+            print("The hit rate is " + str(hit / count))
+            return np.array(retl)
+        
+#             return np.array([
+#                 np.mean([self.word2vec[word] for word in doc if word in self.word2vec]
+#                         or [np.zeros(self.dim)], axis=0)
+#                 for doc in X
+#             ])
+    
+    print('featurizeChinese...')
+    vectorizer = MeanEmbeddingVectorizer(w2v, 300)
+    X = vectorizer.transform(docs)
+    return X
+
+
+def loadWordVectors(fname, size=float('inf')):
+    """param: fname, file name of word embeddings
+    size: for max loaded lines restriction
+    return: a dictionary storing the embeddings"""
+    print('loadWordVectors:' + fname + '...')
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    print(n, d)  # n is #words, d is dimension
+    # fin.readline()
+    data = {}
+    cur = 0
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = np.asarray(list(map(float, tokens[1:])))  # Yixin: added np.asarray(list())
+        if (cur > size): break 
+        else: cur += 1
+    return data
+
+
 def classifiers(X_train, y_train, X_test, y_test):
     """param: X_train, y_train are feature vectors for training
     param: X_test, y_test are feature vectors for testing"""
@@ -188,75 +348,8 @@ def classifiers(X_train, y_train, X_test, y_test):
 #     classifier(MultinomialNB(), "MultinomialNB")
 
 
-def chineseWordSegmentation():
-    thu = thulac.thulac(seg_only=True)  # segment only
-    # sport 0-131603.txt
-    # politics 339764-402849.txt
-    # science 481650-644578.txt
-    for i in range(50000, 60000):
-        if (i/10) == int(i/10): print(i)
-        filename = str(i)
-        # TODO pick 1000 files only for speed
-        thu.cut_f("data/sport/" + filename + ".txt", "data/sport_out/" + filename + ".txt")
-        thu.cut_f("data/politics/3" + filename + ".txt", "data/politics_out/3" + filename + ".txt")
-        thu.cut_f("data/science/5" + filename + ".txt", "data/science_out/5" + filename + ".txt")
-
-
-def tokenizeChinese():
-
-    def read_files(file_name, target, X, y):
-        """param: file_name, string type file name
-        param: target, int for target label
-        param: X to append
-        param: y to append"""
-        for file in os.listdir(file_name):
-            doc = []
-            file = open(file_name + "/" + file, 'r')
-            for line in file:
-                l = line.split()
-                doc = doc + l
-            X.append(doc)
-            y.append(target)
-
-    print('tokenizeChinese...')
-    X = []
-    y = []
-    read_files("data/sport_out", 0, X, y)
-    read_files("data/politics_out", 1, X, y)
-    read_files("data/science_out", 2, X, y)
-    return (X, y)
-
-
-def featurizeChinese(docs, w2v):
-    """param: docs, a list of lists of tokens (strings)
-    w2v, a dictionary of word embeddings
-    return: a matrix, a row represents a doc's feature vector"""
-    
-    class MeanEmbeddingVectorizer(object):
-
-        def __init__(self, word2vec, dim=300):
-            self.word2vec = word2vec
-            # if a text is empty we should return a vector of zeros
-            # with the same dimensionality as all the other vectors
-    #         self.dim = len(word2vec.itervalues().next())
-            self.dim = dim
-    
-        def fit(self, X):
-            return self
-    
-        def transform(self, X):
-            return np.array([
-                np.mean([self.word2vec[w] for w in words if w in self.word2vec]
-                        or [np.zeros(self.dim)], axis=0)
-                for words in X
-            ])
-    
-    print('featurizeChinese...')
-    vectorizer = MeanEmbeddingVectorizer(w2v, 300)
-    X = vectorizer.transform(docs)
-    return X
-
-
 if __name__ == '__main__':
     main()
-#     chineseWordSegmentation()
+    # run only once to get output
+#     THUWordSegmentation()
+#     UMWordSegmentation(["Bi-Laws", "Bi-Thesis"], 100000)
